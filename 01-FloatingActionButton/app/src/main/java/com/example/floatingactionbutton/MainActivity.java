a58e9d065cc2232f285fde8bf13861b8a2270ccd
Andi Rustianto <anditodev46@gmail.com>
2024-01-23 23:35:21 +0700

    .

--- 01-FloatingActionButton/app/src/main/java/com/example/floatingactionbutton/MainActivity.java
+++ 01-FloatingActionButton/app/src/main/java/com/example/floatingactionbutton/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.floatingactionbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton btn_fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_fab = findViewById(R.id.fab);
+        btn_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You have clicked on floating button", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
