2ab57feb73a9ae929acbc4a996026f1bc75c69b5
Andi Rustianto <anditodev46@gmail.com>
2024-01-26 18:11:57 +0700

    new activity

---
+++ OpenNewActivity/app/src/main/java/com/example/opennewactivity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.opennewactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button goToNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Open New Act");
+        goToNext = findViewById(R.id.btn_next);
+
+        goToNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
